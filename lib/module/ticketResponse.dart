39cfd228252134b13809d32576d342ea112e9ca0
Ashen Madhusanka <99417273+ashen0909@users.noreply.github.com>
2023-08-02 10:57:05 +0530

    update

--- lib/module/ticketResponse.dart
+++ lib/module/ticketResponse.dart
@@ -4,84 +4,92 @@
 
 import 'dart:convert';
 
-TicketResponse ticketResponseFromJson(String str) => TicketResponse.fromJson(json.decode(str));
+TicketResponse ticketResponseFromJson(String str) =>
+    TicketResponse.fromJson(json.decode(str));
 
 String ticketResponseToJson(TicketResponse data) => json.encode(data.toJson());
 
 class TicketResponse {
-    List<Body> body;
-    String status;
-    String message;
+  List<Body> body;
+  String status;
+  String message;
 
   var ticketResponse;
 
-    TicketResponse({
-        required this.body,
-        required this.status,
-        required this.message,
-    });
+  TicketResponse({
+    required this.body,
+    required this.status,
+    required this.message,
+  });
 
-    factory TicketResponse.fromJson(Map<String, dynamic> json) => TicketResponse(
+  factory TicketResponse.fromJson(Map<String, dynamic> json) => TicketResponse(
         body: List<Body>.from(json["body"].map((x) => Body.fromJson(x))),
         status: json["status"],
         message: json["message"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "body": List<dynamic>.from(body.map((x) => x.toJson())),
         "status": status,
         "message": message,
-    };
+      };
 }
 
 class Body {
-    int? id;
-    String? title;
-    String? description;
-    String? type;
-    String? status;
-    DateTime? createdDate;
-    dynamic userSubmittedCreatedDate;
-    DateTime? updatedDate;
-    DateTime? closedDate;
-    String? closureComments;
-    User? user;
-    List<Comment> comments;
-    List<dynamic> history;
+  int? id;
+  String? title;
+  String? description;
+  String? type;
+  String? status;
+  DateTime? createdDate;
+  dynamic userSubmittedCreatedDate;
+  DateTime? updatedDate;
+  DateTime? closedDate;
+  String? closureComments;
+  User? user;
+  List<Comment> comments;
+  List<dynamic> history;
 
-    Body({
-        required this.id,
-        this.title,
-        required this.description,
-        this.type,
-        required this.status,
-        this.createdDate,
-        this.userSubmittedCreatedDate,
-        this.updatedDate,
-        this.closedDate,
-        this.closureComments,
-        this.user,
-        required this.comments,
-        required this.history,
-    });
+  Body({
+    required this.id,
+    this.title,
+    required this.description,
+    this.type,
+    required this.status,
+    this.createdDate,
+    this.userSubmittedCreatedDate,
+    this.updatedDate,
+    this.closedDate,
+    this.closureComments,
+    this.user,
+    required this.comments,
+    required this.history,
+  });
 
-    factory Body.fromJson(Map<String, dynamic> json) => Body(
+  factory Body.fromJson(Map<String, dynamic> json) => Body(
         id: json["id"],
         title: json["title"],
         description: json["description"],
         type: json["type"],
         status: json["status"],
-        createdDate: json["createdDate"] == null ? null : DateTime.parse(json["createdDate"]),
+        createdDate: json["createdDate"] == null
+            ? null
+            : DateTime.parse(json["createdDate"]),
         userSubmittedCreatedDate: json["userSubmittedCreatedDate"],
-        updatedDate: json["updatedDate"] == null ? null : DateTime.parse(json["updatedDate"]),
-        closedDate: json["closedDate"] == null ? null : DateTime.parse(json["closedDate"]),
+        updatedDate: json["updatedDate"] == null
+            ? null
+            : DateTime.parse(json["updatedDate"]),
+        closedDate: json["closedDate"] == null
+            ? null
+            : DateTime.parse(json["closedDate"]),
         closureComments: json["closureComments"],
         user: json["user"] == null ? null : User.fromJson(json["user"]),
-        comments: List<Comment>.from(json["comments"].map((x) => Comment.fromJson(x))),
+        comments: List<Comment>.from(
+            json["comments"].map((x) => Comment.fromJson(x))),
         history: List<dynamic>.from(json["history"].map((x) => x)),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "title": title,
         "description": description,
@@ -95,85 +103,85 @@ class Body {
         "user": user?.toJson(),
         "comments": List<dynamic>.from(comments.map((x) => x.toJson())),
         "history": List<dynamic>.from(history.map((x) => x)),
-    };
+      };
 }
 
 class Comment {
-    int id;
-    String comment;
-    DateTime createdDate;
-    int userId;
-    String userName;
+  int id;
+  String comment;
+  DateTime createdDate;
+  int userId;
+  String userName;
 
-    Comment({
-        required this.id,
-        required this.comment,
-        required this.createdDate,
-        required this.userId,
-        required this.userName,
-    });
+  Comment({
+    required this.id,
+    required this.comment,
+    required this.createdDate,
+    required this.userId,
+    required this.userName,
+  });
 
-    factory Comment.fromJson(Map<String, dynamic> json) => Comment(
+  factory Comment.fromJson(Map<String, dynamic> json) => Comment(
         id: json["id"],
         comment: json["comment"],
         createdDate: DateTime.parse(json["createdDate"]),
         userId: json["userId"],
         userName: json["userName"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "comment": comment,
         "createdDate": createdDate.toIso8601String(),
         "userId": userId,
         "userName": userName,
-    };
+      };
 }
 
 class User {
-    int id;
-    String email;
-    String firstName;
-    String lastName;
-    dynamic otherNames;
-    String primaryContactNo;
-    dynamic secondaryContactNo;
-    String userStatus;
-    dynamic gender;
-    String nicNumber;
-    DateTime dateOfBirth;
-    DateTime joinDate;
-    dynamic leftDate;
-    DateTime createdDate;
-    Address address;
-    dynamic bankDetails;
-    dynamic designation;
-    dynamic department;
-    List<Role> roles;
+  int id;
+  String email;
+  String firstName;
+  String lastName;
+  dynamic otherNames;
+  String primaryContactNo;
+  dynamic secondaryContactNo;
+  String userStatus;
+  dynamic gender;
+  String nicNumber;
+  DateTime dateOfBirth;
+  DateTime joinDate;
+  dynamic leftDate;
+  DateTime createdDate;
+  Address address;
+  dynamic bankDetails;
+  dynamic designation;
+  dynamic department;
+  List<Role> roles;
 
-    User({
-        required this.id,
-        required this.email,
-        required this.firstName,
-        required this.lastName,
-        this.otherNames,
-        required this.primaryContactNo,
-        this.secondaryContactNo,
-        required this.userStatus,
-        this.gender,
-        required this.nicNumber,
-        required this.dateOfBirth,
-        required this.joinDate,
-        this.leftDate,
-        required this.createdDate,
-        required this.address,
-        this.bankDetails,
-        this.designation,
-        this.department,
-        required this.roles,
-    });
+  User({
+    required this.id,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    this.otherNames,
+    required this.primaryContactNo,
+    this.secondaryContactNo,
+    required this.userStatus,
+    this.gender,
+    required this.nicNumber,
+    required this.dateOfBirth,
+    required this.joinDate,
+    this.leftDate,
+    required this.createdDate,
+    required this.address,
+    this.bankDetails,
+    this.designation,
+    this.department,
+    required this.roles,
+  });
 
-    factory User.fromJson(Map<String, dynamic> json) => User(
+  factory User.fromJson(Map<String, dynamic> json) => User(
         id: json["id"],
         email: json["email"],
         firstName: json["firstName"],
@@ -193,9 +201,9 @@ class User {
         designation: json["designation"],
         department: json["department"],
         roles: List<Role>.from(json["roles"].map((x) => Role.fromJson(x))),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "email": email,
         "firstName": firstName,
@@ -206,90 +214,94 @@ class User {
         "userStatus": userStatus,
         "gender": gender,
         "nicNumber": nicNumber,
-        "dateOfBirth": "${dateOfBirth.year.toString().padLeft(4, '0')}-${dateOfBirth.month.toString().padLeft(2, '0')}-${dateOfBirth.day.toString().padLeft(2, '0')}",
-        "joinDate": "${joinDate.year.toString().padLeft(4, '0')}-${joinDate.month.toString().padLeft(2, '0')}-${joinDate.day.toString().padLeft(2, '0')}",
+        "dateOfBirth":
+            "${dateOfBirth.year.toString().padLeft(4, '0')}-${dateOfBirth.month.toString().padLeft(2, '0')}-${dateOfBirth.day.toString().padLeft(2, '0')}",
+        "joinDate":
+            "${joinDate.year.toString().padLeft(4, '0')}-${joinDate.month.toString().padLeft(2, '0')}-${joinDate.day.toString().padLeft(2, '0')}",
         "leftDate": leftDate,
-        "createdDate": "${createdDate.year.toString().padLeft(4, '0')}-${createdDate.month.toString().padLeft(2, '0')}-${createdDate.day.toString().padLeft(2, '0')}",
+        "createdDate":
+            "${createdDate.year.toString().padLeft(4, '0')}-${createdDate.month.toString().padLeft(2, '0')}-${createdDate.day.toString().padLeft(2, '0')}",
         "address": address.toJson(),
         "bankDetails": bankDetails,
         "designation": designation,
         "department": department,
         "roles": List<dynamic>.from(roles.map((x) => x.toJson())),
-    };
+      };
 }
 
 class Address {
-    int id;
-    String addressLine1;
-    String addressLine2;
-    String city;
-    String province;
+  int id;
+  String addressLine1;
+  String addressLine2;
+  String city;
+  String province;
 
-    Address({
-        required this.id,
-        required this.addressLine1,
-        required this.addressLine2,
-        required this.city,
-        required this.province,
-    });
+  Address({
+    required this.id,
+    required this.addressLine1,
+    required this.addressLine2,
+    required this.city,
+    required this.province,
+  });
 
-    factory Address.fromJson(Map<String, dynamic> json) => Address(
+  factory Address.fromJson(Map<String, dynamic> json) => Address(
         id: json["id"],
         addressLine1: json["addressLine1"],
         addressLine2: json["addressLine2"],
         city: json["city"],
         province: json["province"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "addressLine1": addressLine1,
         "addressLine2": addressLine2,
         "city": city,
         "province": province,
-    };
+      };
 }
 
 class Role {
-    int id;
-    String name;
-    List<Privilege> privileges;
+  int id;
+  String name;
+  List<Privilege> privileges;
 
-    Role({
-        required this.id,
-        required this.name,
-        required this.privileges,
-    });
+  Role({
+    required this.id,
+    required this.name,
+    required this.privileges,
+  });
 
-    factory Role.fromJson(Map<String, dynamic> json) => Role(
+  factory Role.fromJson(Map<String, dynamic> json) => Role(
         id: json["id"],
         name: json["name"],
-        privileges: List<Privilege>.from(json["privileges"].map((x) => Privilege.fromJson(x))),
-    );
+        privileges: List<Privilege>.from(
+            json["privileges"].map((x) => Privilege.fromJson(x))),
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
         "privileges": List<dynamic>.from(privileges.map((x) => x.toJson())),
-    };
+      };
 }
 
 class Privilege {
-    int id;
-    String name;
+  int id;
+  String name;
 
-    Privilege({
-        required this.id,
-        required this.name,
-    });
+  Privilege({
+    required this.id,
+    required this.name,
+  });
 
-    factory Privilege.fromJson(Map<String, dynamic> json) => Privilege(
+  factory Privilege.fromJson(Map<String, dynamic> json) => Privilege(
         id: json["id"],
         name: json["name"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
-    };
+      };
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,148 +1,3 @@
-
-// import 'dart:convert';
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:stomp_dart_client/stomp.dart';
-// import 'package:stomp_dart_client/stomp_config.dart';
-// import 'package:stomp_dart_client/stomp_frame.dart';
-// import 'package:ticketapp/module/ticketResponse.dart';
-// import 'package:web_socket_channel/io.dart';
-// import 'package:web_socket_channel/web_socket_channel.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Ticket App',
-//       home: Home(),
-//     );
-//   }
-// }
-
-// class Home extends StatefulWidget {
-//   const Home({super.key});
-
-//   @override
-//   State<Home> createState() => _HomeState();
-
-//   static getInstance() {}
-// }
-
-// class _HomeState extends State<Home> {
-//   String? jwtToken;
-//   List<Body> ticketList = [];
-//   bool isWebSocketConnected = false;
-//   StompClient? stompClient;
-//   String webSocketMessage = '';
-
-//   getAllTickets() async {
-//     var url = Uri.parse("http://localhost:8080/tickets");
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     jwtToken = prefs.getString('jwtToken');
-
-//     var response = await http.get(
-//       url,
-//       headers: {
-//         "Content-Type": "application/json",
-//         "Authorization": "Bearer $jwtToken",
-//       },
-//     );
-
-//     if (response.statusCode == 200) {
-//       var ticketResponse = ticketResponseFromJson(response.body);
-
-//       setState(() {
-//         ticketList = ticketResponse.body;
-//       });
-//       print("ticketlist:" + ticketList.toString());
-//     } else {
-//       throw Exception('Failed to load tickets');
-//     }
-//   }
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     getAllTickets();
-//     _connectToWebSocket();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Ticket List'),
-//       ),
-//       body: Container(
-//         height: 500,
-//         child: ListView.builder(
-//           itemCount: ticketList.length + 1, 
-//           itemBuilder: (context, index) {
-           
-//             if (index == 0) {
-//               return ListTile(
-//                 title: Text('Notification Message'),
-//                 subtitle: Text(webSocketMessage),
-//               );
-//             }
-
-            
-//             final ticketIndex = index - 1; 
-//             return ListTile(
-//               title: Text(ticketList[ticketIndex].title ?? ''),
-//               subtitle: Text(ticketList[ticketIndex].description),
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-
-//   void _connectToWebSocket() {
-//     stompClient = StompClient(
-//       config: StompConfig(
-//         url: 'ws://localhost:8080/socket',
-//         onConnect: (StompFrame frame) async {
-//           print('connected');
-//           setState(() {
-//             isWebSocketConnected = true;
-//           });
-//           _subscribeToTopic();
-//         },
-//         onWebSocketError: (dynamic error) => print(error.toString()),
-//       ),
-//     );
-//     stompClient!.activate();
-//   }
-
-//   void _subscribeToTopic() {
-//     stompClient?.subscribe(
-//       destination: '/notifications',
-//       headers: {},
-//       callback: (StompFrame frame) {
-     
-//         print("Received message: ${frame.body}");
-      
-//         setState(() {
-//           webSocketMessage = frame.body ?? '';
-       
-//           var jsonData = json.decode(frame.body ?? '');
-//           var newTicket = Body.fromJson(jsonData); 
-
-       
-//           ticketList.insert(0, newTicket); 
-//         });
-//       },
-//     );
-//   }
-// }
-
-
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -166,11 +21,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ticket App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        hintColor: Colors.amber,
-        fontFamily: 'Roboto',
-      ),
       home: Home(),
     );
   }
@@ -188,7 +38,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   String? jwtToken;
   List<Content> ticketListPaged = [];
-   List<Body> ticketList = [];
+  List<Body> ticketList = [];
 
   bool isWebSocketConnected = false;
   StompClient? stompClient;
@@ -197,43 +47,43 @@ class _HomeState extends State<Home> {
 
   int currentPage = 1;
   int offset = 10;
-           
-// getpage function 
+// getpage function //
 
   Future<PageResponse> getTicketsPage(int page, int offset) async {
-  var url = Uri.parse("http://localhost:8080/tickets/page?page=$page&offset=$offset");
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  jwtToken = prefs.getString('jwtToken');
+    var url = Uri.parse(
+        "http://localhost:8080/tickets/page?page=$page&offset=$offset");
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    jwtToken = prefs.getString('jwtToken');
 
-  var response = await http.get(
-    url,
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer $jwtToken",
-    },
-  );
+    var response = await http.get(
+      url,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $jwtToken",
+      },
+    );
 
-  if (response.statusCode == 200) {
-    var pageResponse = PageResponse.fromJson(json.decode(response.body));
-    return pageResponse;
-  } else {
-    throw Exception('mukuth naaaaaaaaaaaaaaaaaaaa');
+    if (response.statusCode == 200) {
+      var pageResponse = PageResponse.fromJson(json.decode(response.body));
+      return pageResponse;
+    } else {
+      throw Exception('mukuth naaaaaaaaaaaaaaaaaaaa');
+    }
   }
-}
 
-
-void fetchTicketsPage(int page, int offset) async {
-  try {
-    var pageResponse = await getTicketsPage(page, offset);
-    setState(() {
-      ticketListPaged = pageResponse.content!;
-    });
-    print("ticketlist:" + pageResponse.toString());
-  } catch (e) {
-    print("Error fetching tickets: $e");
+  void fetchTicketsPage(int page, int offset) async {
+    try {
+      var pageResponse = await getTicketsPage(page, offset);
+      setState(() {
+        ticketListPaged = pageResponse.content!;
+      });
+      print("ticketlist:" + pageResponse.toString());
+    } catch (e) {
+      print("Error fetching tickets: $e");
+    }
   }
-}
- void _fetchNextPage() {
+
+  void _fetchNextPage() {
     currentPage++;
     fetchTicketsPage(currentPage, offset);
   }
@@ -244,8 +94,6 @@ void fetchTicketsPage(int page, int offset) async {
       fetchTicketsPage(currentPage, offset);
     }
   }
-
-
 
   getAllTickets() async {
     var url = Uri.parse("http://localhost:8080/tickets");
@@ -281,68 +129,84 @@ void fetchTicketsPage(int page, int offset) async {
     int offset = 10;
     fetchTicketsPage(page, offset);
     // getTicketsPage(page, offset)
-
- 
   }
 
+  // void initState() {
+  //   super.initState();
+  //   getAllTickets();
+  //   _connectToWebSocket();
+  //   // showToast();
+  // }
+  // @override
+  // void dispose() {
+  //   getAllTickets();
+  //   _connectToWebSocket();
+  //   super.dispose();
+  // }
 
-    @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Ticket List',style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Notification Message',
-              style: TextStyle(fontSize: 18),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Text(
-              webSocketMessage,
-              style: TextStyle(fontSize: 16),
-            ),
-          ),
-          SizedBox(height: 20),
-          Expanded(
-            child: ListView.builder(
-              itemCount: ticketList.length,
-              itemBuilder: (context, index) {
-                final ticket = ticketList[index];
-                final colorIndex = index % 4;
+  @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Ticket List',style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+    ),
+    body: Column(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.8,
+          child: ListView.builder(
+            itemCount: ticketListPaged.length + 1,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                if (webSocketMessage.isNotEmpty) {
+                  showToastNotification(webSocketMessage);
+                  // snack(webSocketMessage);
+                }
+                return Container();
+              }
 
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    color: _getColor(colorIndex),
-                    child: ListTile(
-                      title: Text(ticket.title ?? '',style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold,color: Color.fromARGB(255, 2, 177, 246))),
-                      subtitle: Text(ticket.description),
-                    ),
+              final ticketIndex = index - 1;
+              final colorIndex = ticketIndex % 4; 
+
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  color: _getColor(colorIndex), 
+                  child: ListTile(
+                    title: Text(ticketListPaged[ticketIndex].title ?? '',style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold,color: Color.fromARGB(255, 2, 177, 246))),
+                    subtitle: Text(ticketListPaged[ticketIndex].description ?? ''),
                   ),
-                );
-              },
-            ),
+                ),
+              );
+            },
           ),
-        ],
-      ),
-    );
-  }
-  Color _getColor(int colorIndex) {
-    List<Color> colors = [
-      Color.fromARGB(255, 202, 205, 206),
-    ];
+        ),
+      ],
+    ),
+    bottomNavigationBar: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        ElevatedButton(
+          onPressed: _fetchPreviousPage,
+          child: Text('Previous Page'),
+        ),
+        ElevatedButton(
+          onPressed: _fetchNextPage,
+          child: Text('Next Page'),
+        ),
+      ],
+    ),
+  );
+}
 
-    return colors[colorIndex % colors.length];
-  }
+Color _getColor(int colorIndex) {
+  List<Color> colors = [
+    Color.fromARGB(255, 202, 205, 206), 
+  ];
+
+  return colors[colorIndex % colors.length];
+}
+
 
   void _connectToWebSocket() {
     stompClient = StompClient(
@@ -367,7 +231,7 @@ void fetchTicketsPage(int page, int offset) async {
       headers: {},
       callback: (StompFrame frame) {
         print("Received message: ${frame.body}");
-
+        getAllTickets();
         setState(() {
           webSocketMessage = frame.body ?? '';
 
@@ -375,10 +239,29 @@ void fetchTicketsPage(int page, int offset) async {
           var newTicket = Body.fromJson(jsonData);
 
           ticketList.insert(0, newTicket);
+
+          // Fluttertoast.showToast(
+          //   msg: (webSocketMessage),
+          //   toastLength: Toast.LENGTH_SHORT,
+          //   gravity: ToastGravity.CENTER,
+          //   backgroundColor: Colors.black,
+          //   textColor: Colors.white,
+          //   fontSize: 16.0,
+          // );
         });
       },
     );
   }
+
+  void showToastNotification(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.black54,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
 }
-
-
